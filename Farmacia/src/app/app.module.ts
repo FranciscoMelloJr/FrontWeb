543d80e39f022c3f0bc44ee982d1518c8c3b0fff
Francisco Mello <franciscomelloxpe@gmail.com>
2019-06-20 23:58:21 -0300

    Base ok

--- Farmacia/src/app/app.module.ts
+++ Farmacia/src/app/app.module.ts
@@ -1,9 +1,25 @@
+import { ToastModule } from 'primeng/toast';
+import { HttpClientModule } from '@angular/common/http';
+import { ClientesModule } from './clientes/clientes.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClientesCadastroComponent } from './clientes/clientes-cadastro/clientes-cadastro.component';
+import { ProdutosCadastroComponent } from './produtos/produtos-cadastro/produtos-cadastro.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {ButtonModule} from 'primeng/button';
-
+import {SidebarModule} from 'primeng/sidebar';
 
 import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
+import { MessageService, ConfirmationService } from 'primeng/api';
+import { ProdutosModule } from './produtos/produtos.module';
+
+
+const rotas:Routes = [
+{path: 'clientes',component:ClientesCadastroComponent},
+{path: 'produtos',component:ProdutosCadastroComponent}
+
+];
 
 @NgModule({
   declarations: [
@@ -11,9 +27,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    ButtonModule
+    BrowserAnimationsModule,
+    ProdutosModule,
+    ClientesModule,
+    HttpClientModule,
+    ToastModule,
+    ButtonModule,
+    SidebarModule,
+    RouterModule.forRoot(rotas)
   ],
-  providers: [],
+  providers: [
+    MessageService,
+    ConfirmationService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
