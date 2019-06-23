d452c34c919e5f4082d8e8b7d79eb4283fa23100
Francisco Mello <franciscomelloxpe@gmail.com>
2019-06-23 20:14:10 -0300

    Vendas Incio

---
+++ Farmacia/src/app/vendas/vendas-cadastro/vendas-cadastro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vendas-cadastro',
+  templateUrl: './vendas-cadastro.component.html',
+  styleUrls: ['./vendas-cadastro.component.css']
+})
+export class VendasCadastroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ Farmacia/src/app/vendas/vendas.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VendasCadastroComponent } from './vendas-cadastro/vendas-cadastro.component';
+
+@NgModule({
+  declarations: [VendasCadastroComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class VendasModule { }
